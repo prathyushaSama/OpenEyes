ecd34d3453773f20b0329cb2c5256dc1a2663727
petergallagher <peter.s.gallagher@gmail.com>
2017-01-12 12:40:17 +0000

    Adds prototype validation for transcripts
    Adds a demo of validation using mutalyzer.nl. This will need to be made
    configurable for if mutalyzer is installed locally or not and refactored out into
    a sensible place and have hardcoded input names removed.

--- protected/modules/Genetics/assets/js/module.js
+++ protected/modules/Genetics/assets/js/module.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
 
+  function errorTranscript($transcript) {
+    $transcript.addClass('error');
+  }
+
+  function validateGeneTranscript() {
+    var $transcript = $(this);
+    $.getJSON('https://192.168.111.222/json/checkSyntax', {variant: $transcript.val()}, function(data){
+      if(data.valid){
+        $transcript.removeClass('error');
+      } else {
+        errorTranscript($transcript);
+      }
+    }).fail(function() {
+      errorTranscript($transcript);
+    });
+  }
+
+  var $pedigreeGeneTranscript = $('#Pedigree_gene_transcript');
+  $pedigreeGeneTranscript.on('change', validateGeneTranscript);
+  $pedigreeGeneTranscript.trigger('change');
 });
