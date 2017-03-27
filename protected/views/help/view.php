16ee0482fcbb28e87a9beffefea00eb2557672aa
marshl <marshl@live.com.au>
2017-03-27 14:21:39 +1100

    Changed the list of pages in the help screen to have list levels

--- protected/views/help/view.php
+++ protected/views/help/view.php
@@ -2,15 +2,15 @@
 <?php
 
 $helplinks = array(
-	0 => array('name' => 'Welcome to OpenEyes', 'view' => 'welcome_to_openeyes'),
-	1 => array('name' => 'What Will Change?', 'view' => 'what_will_change'),
-	3 => array('name' => 'What OpenEyes Cannot Yet Do', 'view' => 'cannot_do'),
-	4 => array('name' => 'Which Patients Will Go Onto OpenEyes?', 'view' => 'which_patients'),
-	5 => array('name' => 'How Are Patients Added to OpenEyes?', 'view' => 'adding_patients'),
-	6 => array('name' => 'How Are Patient Details Maintained in OpenEyes?', 'view' => 'patient_details'),
-	7 => array('name' => 'Getting Started', 'view' => 'getting_started'),
-	8 => array('name' => 'Creating a New Patient', 'view' => 'patient_create'),
-	9 => array('name' => 'Editing Patient Details', 'view' => 'edit_patient'),
+	0 => array('name' => 'Welcome to OpenEyes', 'view' => 'welcome_to_openeyes', 'level' => 1),
+	1 => array('name' => 'What Will Change?', 'view' => 'what_will_change', 'level' => 2),
+	2 => array('name' => 'What OpenEyes Cannot Yet Do', 'view' => 'cannot_do', 'level' => 2),
+	3 => array('name' => 'Which Patients Will Go Onto OpenEyes?', 'view' => 'which_patients', 'level' => 2),
+	4 => array('name' => 'How Are Patients Added to OpenEyes?', 'view' => 'adding_patients', 'level' => 2),
+	5 => array('name' => 'How Are Patient Details Maintained in OpenEyes?', 'view' => 'patient_details', 'level' => 2),
+	6 => array('name' => 'Getting Started', 'view' => 'getting_started', 'level' => 1),
+	7 => array('name' => 'Creating a New Patient', 'view' => 'patient_create', 'level' => 2),
+	8 => array('name' => 'Editing Patient Details', 'view' => 'edit_patient', 'level' => 2),
 );
 ?>
 <br/>
@@ -20,10 +20,18 @@ $helplinks = array(
         <h1>Help Topics</h1>
             <ul>
             <?php foreach($helplinks as $id => $arr): ?>
+                <?php if(array_key_exists($id - 1, $helplinks) && $helplinks[$id - 1]['level'] < $arr['level']): ?>
+                    <ul>
+                <?php endif; ?>
                 <?php if($id == $topic_id): ?>
                     <li><?php echo $arr['name']; ?></li>
                 <?php else: ?>
                     <li><?php echo CHtml::link($arr['name'], array('/help/view/' . $id)); ?></li>
+                <?php endif; ?>
+
+                <?php if(!array_key_exists($id + 1, $helplinks) && $arr['level'] == 2
+                		|| $arr['level'] == 2 && array_key_exists($id + 1, $helplinks) && $helplinks[$id + 1]['level'] < $arr['level']):?>
+                    </ul>
                 <?php endif; ?>
             <?php endforeach; ?>
             </ul>
