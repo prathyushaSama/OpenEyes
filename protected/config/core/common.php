6b56b1251543a83603675bee2d4dd589bec2a39c
Liam Marshall <marshl@live.com.au>
2017-05-29 14:39:10 +1000

    Added OETrial to the list of modules

--- protected/config/core/common.php
+++ protected/config/core/common.php
@@ -61,6 +61,7 @@ return array(
             'parameters' => array('PatientAge'),
             'providers' => array('mysql' => 'DBProvider')
         ),
+        'OETrial',
         'Genetics'
     ),
 
