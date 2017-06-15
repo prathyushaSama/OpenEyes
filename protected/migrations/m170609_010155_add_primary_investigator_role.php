d29b6bc4303a7b43483d56247647c676598a7ecd
Liam Marshall <marshl@live.com.au>
2017-06-16 08:23:09 +1000

    Changed the add_primary_investigaor_role migration script to use safeUp/safeDown instead of up/down

--- protected/migrations/m170609_010155_add_primary_investigator_role.php
+++ protected/migrations/m170609_010155_add_primary_investigator_role.php
@@ -4,12 +4,12 @@ class m170609_010155_add_primary_investigator_role extends CDbMigration
 {
     const PRIMARY_INVESTIGATOR_ROLE = "Primary Investigator";
 
-    public function up()
+    public function safeUp()
     {
         $this->insert('authitem', array('name' => self::PRIMARY_INVESTIGATOR_ROLE, 'type' => 2));
     }
 
-    public function down()
+    public function safeDown()
     {
         $this->delete('authitem', 'name = "' . self::PRIMARY_INVESTIGATOR_ROLE . '"');
     }
